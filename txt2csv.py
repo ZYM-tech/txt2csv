e0b0b5c9bfc97cefa11ffb8fb48a06c62cfce382
ZYM <yiming.zhang1024@gmail.com>
2020-04-12 22:39:34 +0800

    update

--- txt2csv.py
+++ txt2csv.py
@@ -1,9 +1,9 @@
 import csv
-csvFile = open("/Users/zhangyiming/PycharmProjects/手写数字识别/20160401.csv",'w',newline='',encoding='utf-8')
+csvFile = open("new_csv",'w',newline='',encoding='utf-8')
 writer = csv.writer(csvFile)
 csvRow = []
 
-f = open("/Users/zhangyiming/PycharmProjects/手写数字识别/20160401.txt",'r',encoding='utf-8')
+f = open("your_txt",'r',encoding='utf-8')
 for line in f:
     csvRow = line.split()
     writer.writerow(csvRow)
